45bae84cfd7ff4894d2d40a711ef19f6c6195dff
Ethan <hello@ethang.email>
2024-08-18 14:11:55 -0500

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-const isProduction = "production" === import.meta.env.MODE;
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: isProduction
-    ? undefined
-    : cloudflare({
-      platformProxy: {
-        enabled: true,
-      },
-    }),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), react()],
-  output: isProduction
-    ? "static"
-    : "server",
+  output: "hybrid",
 });
