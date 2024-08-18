dbb9bd43f833cf39b46b7f79d5a6a0b5d5542687
Ethan <hello@ethang.email>
2024-08-18 14:05:37 -0500

    hide sign in

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,7 +1,7 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/navbar";
-import { NavbarMenu, NavbarMenuToggle } from "@nextui-org/react";
-import { useState } from "react";
+import { NavbarBrand, NavbarMenu, NavbarMenuToggle } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 import { NavigationItems } from "./navigation-items.tsx";
 
@@ -10,8 +10,19 @@ type NavigationProperties = {
   readonly currentPathname: string;
 };
 
+// eslint-disable-next-line max-lines-per-function
 export function Navigation({ currentPathname }: NavigationProperties) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLocal, setIsLocal] = useState(false);
+
+  useEffect(() => {
+    if ("undefined" === typeof window) {
+      return;
+    }
+
+    // eslint-disable-next-line react/hooks-extra/no-direct-set-state-in-use-effect
+    setIsLocal("localhost" === location.hostname);
+  }, []);
 
   return (
     <Navbar
@@ -25,6 +36,9 @@ export function Navigation({ currentPathname }: NavigationProperties) {
             : "Open menu"}
           className="sm:hidden"
         />
+        <NavbarBrand className="text-xl font-bold">
+          Ethan Glover
+        </NavbarBrand>
       </NavbarContent>
       <NavbarContent
         className="hidden sm:flex"
@@ -33,14 +47,16 @@ export function Navigation({ currentPathname }: NavigationProperties) {
         <NavigationItems currentPathname={currentPathname} />
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </NavbarItem>
+        {isLocal && (
+          <NavbarItem>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </NavbarItem>
+        )}
       </NavbarContent>
       <NavbarMenu className="z-40">
         <NavigationItems currentPathname={currentPathname} />
