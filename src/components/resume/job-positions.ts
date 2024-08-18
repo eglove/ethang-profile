42827f23bf2a9a164d178493165940a6f3229828
Ethan <hello@ethang.email>
2024-08-18 12:51:39 -0500

    fix build

--- src/components/resume/job-positions.ts
+++ src/components/resume/job-positions.ts
@@ -3,6 +3,8 @@ import isNil from "lodash/isNil";
 import map from "lodash/map";
 import { DateTime } from "luxon";
 
+import type { GetJobsJson } from "../../pages/api/job.ts";
+
 import { neonSql } from "../../clients/neon.ts";
 
 const listFormatter = new Intl.ListFormat(undefined, {
@@ -11,14 +13,14 @@ const listFormatter = new Intl.ListFormat(undefined, {
 
 export async function getJobPositions() {
   const jobs = await neonSql`select * from "Job"
-    order by "endDate" DESC`;
+    order by "endDate" DESC` as GetJobsJson;
 
 
   return map(jobs, (job) => {
-    const startDate = DateTime.fromJSDate(job.startDate).toFormat("MM/yy");
+    const startDate = DateTime.fromJSDate(new Date(job.startDate)).toFormat("MM/yy");
     let endDate = "(Current)";
     if (!isNil(job.endDate)) {
-      endDate = DateTime.fromJSDate(job.endDate).toFormat("MM/yy");
+      endDate = DateTime.fromJSDate(new Date(job.endDate)).toFormat("MM/yy");
     }
 
     return new Paragraph({
