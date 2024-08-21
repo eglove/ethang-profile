62918af46eee5817b684ee53fa374b9f0aff9f15
Ethan <hello@ethang.email>
2024-08-21 08:42:48 -0500

    uses master

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
-await projectBuilder("ethang-profile", "main", {
+await projectBuilder("ethang-profile", "master", {
   isLibrary: false,
   scripts: ["UPDATE", "DEDUPE", "LINT", "BUILD"],
 });
