64a527387368c7342fc0110024234fb1f3590293
Ethan <hello@ethang.email>
2024-08-28 18:16:14 -0500

    Add post-install script and update dependencies

--- build.mjs
+++ build.mjs
@@ -1,7 +1,12 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
+import { execSync } from "node:child_process";
 
 await projectBuilder("ethang-profile", "master", {
   isLibrary: false,
+  postInstall() {
+    // eslint-disable-next-line sonar/no-os-command-from-path
+    execSync("pnpm prismaGenerate");
+  },
   scripts: ["UPDATE", "DEDUPE", "LINT", "BUILD"],
 });
 
