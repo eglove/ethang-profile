5f362b20013f03a84ff7a724d5f49b77f2c0e38b
Ethan <hello@ethang.email>
2024-10-11 09:07:27 -0500

    dedupe

--- build.mjs
+++ build.mjs
@@ -2,6 +2,6 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("ethang-profile", "master", {
   isLibrary: false,
-  scripts: ["pnpm up -i --latest", "pnpm prismaGenerate", "pnpm lint", "pnpm build"],
+  scripts: ["pnpm up -i --latest", "pnpm dedupe", "pnpm prismaGenerate", "pnpm lint", "pnpm build"],
 });
 
